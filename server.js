8fe3e5a670038753aea64275a936da44a3e88c07
SaraCoolman <56956632+SaraCoolman@users.noreply.github.com>
2022-06-16 20:38:03 +0200

    deployment

--- server.js
+++ server.js
@@ -1,24 +1,65 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const db = require("./app/models");
-db.sequelize.sync({ force: true }).then(() => {
-  console.log("Drop and re-sync db.");
-});
-var corsOptions = {
-  origin: "http://localhost:8081"
-};
-app.use(cors(corsOptions));
+
+app.use(cors({
+	'allowedHeaders': ['Content-Type'], // headers that React is sending to the API
+	'exposedHeaders': ['Content-Type'], // headers that you are sending back to React
+	'origin': '*',
+	'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+	'preflightContinue': false
+
+}));
+
+// Add Access Control Allow Origin headers
+
+
+
+//var corsOptions = {
+	//origin: "http://localhost:8081"
+//};
+
+
+
+//app.use(cors(corsOptions));
 // parse requests of content-type - application/json
 app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 // simple route
+
+
+const db = require("./app/models");
+require("./app/routes/lodge.routes")(app);
+require("./app/routes/person.routes")(app); 
+db.sequelize.sync();
+
+
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to UkraineLodgeService" });
+	res.json({ message: "Welcome to the Ukraine Lodge Service" });
 });
+//require("./app/routes/tutorial.routes")(app);
+
+
+
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+
+// Add Access Control Allow Origin headers
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+
+	);
+	next();
 });
+
+
+
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}.`);
+})
